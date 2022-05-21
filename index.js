7bc651f498499e128e0302705e0acb03fb03ed12
andersonew <85598501+andersonew@users.noreply.github.com>
2022-05-21 17:06:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const api = require("./api");
+const express = require("express");
+const cors = require("cors")
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.listen(3000);
+
+server.get('/pokemon/:id' , async (req, res) => {
+    const { id } = req.params;
+    try {
+        const { data } = await api.get(`pokemon/${id}`);
+
+      return res.send({ name: data.name });
+    } catch (error) {
+        res.send({ error: error.message });
+
+    }
+} );
+
