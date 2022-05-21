f3144847bd9a597c215efc2b917f698ed7caa118
andersonew <85598501+andersonew@users.noreply.github.com>
2022-05-21 17:23:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const api = require("./api");
 const express = require("express");
 const cors = require("cors")
 
-const server = express();
+let server = express();
 
 server.use(express.json());
 server.use(cors());
